4bf05f01ed5413904fa169e8eafbc955756d0d8f
DAIUL <daiul@DESKTOP-9CGU3O5.localdomain>
2023-10-25 00:24:16 +0200

    c'est quoi ce mutex de golmon

--- philo/philo.h
+++ philo/philo.h
@@ -8,7 +8,13 @@
 # include <pthread.h>
 # include <unistd.h>
 
-
+typedef struct s_mut
+{
+    pthread_mutex_t *eat;
+    pthread_mutex_t *sleep;
+    pthread_mutex_t *think;
+    pthread_mutex_t *fork;
+}               t_mut;
 
 void    *routine(void *osef);
 void    threads_test(void);
