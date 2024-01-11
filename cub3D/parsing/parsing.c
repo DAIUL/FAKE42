a178ccefb5d550a7646942602504ddca5fd98e22
DAIUL <quentin.puigsp@gmail.com>
2024-01-11 17:20:54 +0100

    oulala le parsing

--- cub3D/parsing/parsing.c
+++ cub3D/parsing/parsing.c
@@ -14,3 +14,59 @@ int	parsing_map(char *map)
 	else
 		return (printf("non\n"), 0);
 }
+
+char	*remove_nl(char *line);
+{
+	int	i;
+	char	*clear;
+
+	i = 0;
+	clear = ft_calloc(ft_strlen(line), sizeof(char));
+	while (line[i] && (line[i] != '\n'))
+		clear[i] = line[i++];
+	free(line);
+	return (clear);
+}
+
+char	**copy_map(char **map, int lect, char **av)
+{
+	int		fd;
+	int		i;
+	char	*line;
+
+	i = 0;
+	fd = open(av, O_RDONLY);
+	while (i < lect)
+	{
+		line = get_next_line(fd);
+		map[i++] = remove_nl(line);
+	}
+	close(fd);
+	map[i] = ft_calloc(1, sizeof(char));
+	return (map);
+}
+
+char	**map_size(int ac, char **av)
+{
+	char	**map;
+	int		lect;
+	int		fd;
+	char	*line;
+
+	fd = open(av[1], O_RDONLY);
+	if (fd < 0)
+		ERROR
+	lect = 0;
+	line = get_next_line(fd);
+	while (line)
+	{
+		free(line);
+		line = get_next_line(fd);
+		lect++;
+	}
+	free(line);
+	map = ft_calloc((lect + 1), sizeof(char *));
+	close(fd);
+	map = copy_map(map, lect, av[1]);
+	return (map);
+}
